1bb0f614659224a00c554d77df6508a091758737
llluper <mitchelljstanford@gmail.com>
2018-11-04 23:30:27 +1100

    Fix Certificate Authority Error seen on UI

--- Startup.cs
+++ Startup.cs
@@ -47,7 +47,6 @@ namespace LoanManagementSystemWebApi
             }
 
             app.UseCors("AllowSpecificOrigin");
-            app.UseHttpsRedirection();
             app.UseMvc();
         }
     }
