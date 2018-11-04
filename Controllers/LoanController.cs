427697cb9dcb6b557828cd0b0082be5222ae924c
llluper <mitchelljstanford@gmail.com>
2018-11-04 20:34:30 +1100

    LoanController added, ConnectionString for SQL database added to appsettings for connection to SQL Server

---
+++ Controllers/LoanController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+using LoanManagementSystemWebApi.Models;
+using Microsoft.Extensions.Configuration;
+using System.Data.SqlClient;
+
+namespace LoanManagementSystemWebApi.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class LoanController : ControllerBase
+  {
+    private readonly LoanContext _context;
+
+    public LoanController(LoanContext context)
+    {
+      _context = context;
+    }
+
+    [HttpGet]
+    public ActionResult<List<LoanItem>> GetAll()
+    {
+      var items = _context.LoanList.ToList();
+      if (items == null)
+      {
+        return NotFound();
+      }
+      return _context.LoanList.ToList();
+    }
+  }
+}
